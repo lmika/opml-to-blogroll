c16f95d2a5ab0297691ec8cb29f96e2544156e46
Leon Mika <lmika@lmika.org>
2024-02-05 21:48:34 +1100

    Some more bug fixes around decoding HTML elements

--- web/scripts/output.js
+++ web/scripts/output.js
@@ -2,6 +2,11 @@ let copyOutputElem = document.querySelector("#copy-output");
 let copyIndicatorElem = document.querySelector("#copy-indicator");
 let outputElem = document.querySelector("#output");
 
+function htmlDecode(input) {
+    let doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+}
+
 function showCopyIndicator(message) {
     copyIndicatorElem.innerText = message;
     copyIndicatorElem.classList.remove("hidden");
@@ -12,7 +17,7 @@ if (!!navigator.clipboard) {
         let outputInnerText = outputElem.innerHTML;
 
         try {
-            await navigator.clipboard.writeText(outputInnerText);
+            await navigator.clipboard.writeText(htmlDecode(outputInnerText));
             showCopyIndicator("Copied");
         } catch (e) {
             console.log(e);
